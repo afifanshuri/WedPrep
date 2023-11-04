a062da63565002c89c96a7463a9fa78c3f2a06ab
Afif Fanshuri <afiffanshuri.s@gmail.com>
2023-11-04 19:32:49 +0800

    add/edit router config

---
+++ src/main/java/com/Apop/WedPrep/common/web/DashboardController.java
@@ -0,0 +1,12 @@
+package com.Apop.WedPrep.common.web;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Level;
+import ch.qos.logback.classic.Logger;
+import java.util.*;
+
+
+public class DashboardController {
+}
---
+++ src/main/java/com/Apop/WedPrep/config/RouterConfig.java
@@ -0,0 +1,11 @@
+package com.Apop.WedPrep.config;
+
+public final class RouterConfig {
+	public static final String FE_URL = "http://localhost:5173";
+	public static final String BASE_URL = "/wedprep/api";
+	public static final String CATERING_BASE_URL = "/catering";
+	public static final String VIEW = "/view";
+	public static final String ADD = "/add";
+	public static final String DELETE = "/delete";
+
+}
